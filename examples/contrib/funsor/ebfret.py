26467886db59cb871d911946b14cc9532be5ac48
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-04-30 16:43:51 -0400

    tested on simulated data

--- examples/contrib/funsor/ebfret.py
+++ examples/contrib/funsor/ebfret.py
@@ -52,6 +52,7 @@ import torch.nn as nn
 from torch.distributions import constraints
 
 from pyro.contrib.examples import polyphonic_data_loader as poly
+from pyro.infer import Predictive
 from pyro.infer.autoguide import AutoDelta
 from pyro.ops.indexing import Vindex
 from pyro.util import ignore_jit_warnings
@@ -83,144 +84,140 @@ log.addHandler(debug_handler)
 #     y[t-1]     y[t]     y[t+1]
 #
 def model(data, num_states, vectorized):
-    with pyro.plate("trace", data.shape[0], dim=-1):
-        pi = pyro.sample("pi", dist.Dirichlet(pyro.param("rho")))
-        with pyro.plate("state", num_states, dim=-2):
-            A = pyro.sample("A", dist.Dirichlet(pyro.param("alpha")))
-            lamda = pyro.sample("lamda", dist.Gamma(pyro.param("a"), pyro.param("b")))
-            mu = pyro.sample(
-                "mu", dist.Normal(pyro.param("m"), pyro.param("beta") * lamda)
+    pyro.param("rho0", torch.ones(num_states), constraint=constraints.positive)
+    pyro.param(
+        "alpha0", torch.ones(num_states, num_states), constraint=constraints.positive
+    )
+    pyro.param("a0", torch.ones(num_states) * 10, constraint=constraints.positive)
+    pyro.param("b0", torch.ones(num_states), constraint=constraints.positive)
+    pyro.param("m0", torch.arange(num_states) / num_states, constraint=constraints.real)
+    pyro.param("beta0", torch.ones(num_states), constraint=constraints.positive)
+
+    with pyro.plate("trace", data.shape[1], dim=-1):
+        pi = pyro.sample("pi", dist.Dirichlet(pyro.param("rho0")))
+        # with pyro.plate("state", num_states, dim=-2):
+        A = pyro.sample("A", dist.Dirichlet(pyro.param("alpha0")).to_event(1))
+        lamda = pyro.sample(
+            "lamda", dist.Gamma(pyro.param("a0"), pyro.param("b0")).to_event(1)
+        )
+        mu = pyro.sample(
+            "mu",
+            dist.Normal(
+                pyro.param("m0"), 1 / (pyro.param("beta0") * lamda).sqrt()
+            ).to_event(1),
+        )
+
+        z_prev = None
+        markov_loop = (
+            pyro.vectorized_markov(name="time", size=data.shape[0], dim=-2)
+            if vectorized
+            else pyro.markov(range(data.shape[0]))
+        )
+        for t in markov_loop:
+            z_curr = pyro.sample(
+                "z_{}".format(t),
+                dist.Categorical(
+                    pi if isinstance(t, int) and t < 1 else Vindex(A)[..., z_prev, :]
+                ),
+                infer={"enumerate": "parallel"},
             )
-
-            z_prev = None
-            markov_loop = (
-                pyro.vectorized_markov(name="time", size=data.shape[1], dim=-2)
-                if vectorized
-                else pyro.markov(range(data.shape[1]))
+            pyro.sample(
+                "x_{}".format(t),
+                dist.Normal(
+                    Vindex(mu)[..., z_curr], 1 / Vindex(lamda)[..., z_curr].sqrt()
+                ),
+                obs=data[t],
             )
-            for t in markov_loop:
-                z_curr = pyro.sample(
-                    "z_{}".format(t),
-                    dist.Categorical(pi if isinstance(t, int) and t < 1 else A[z_prev]),
-                    infer={"enumerate": "parallel"}
-                )
-                pyro.sample(
-                    "x_{}".format(t),
-                    dist.Normal(Vindex(mu)[..., z_curr], Vindex(lamda)[..., z_curr]),
-                    obs=data[t],
-                )
-                z_prev = z_curr
+            z_prev = z_curr
 
 
-models = {
-    name[len("model_") :]: model
-    for name, model in globals().items()
-    if name.startswith("model_")
-}
+def guide(data, num_states, vectorized):
+    data_dim = data.shape[1]
+    pyro.param("rho", torch.ones(data_dim, num_states), constraint=constraints.positive)
+    pyro.param(
+        "alpha",
+        torch.ones(data_dim, num_states, num_states),
+        constraint=constraints.positive,
+    )
+    pyro.param(
+        "a", torch.ones(data_dim, num_states) * 10, constraint=constraints.positive
+    )
+    pyro.param("b", torch.ones(data_dim, num_states), constraint=constraints.positive)
+    pyro.param(
+        "m",
+        torch.arange(num_states).repeat(data_dim, 1) / num_states,
+        constraint=constraints.real,
+    )
+    pyro.param(
+        "beta", torch.ones(data_dim, num_states), constraint=constraints.positive
+    )
+
+    with pyro.plate("trace", data.shape[1], dim=-1):
+        pyro.sample("pi", dist.Dirichlet(pyro.param("rho")))
+        pyro.sample("A", dist.Dirichlet(pyro.param("alpha")).to_event(1))
+        lamda = pyro.sample(
+            "lamda", dist.Gamma(pyro.param("a"), pyro.param("b")).to_event(1)
+        )
+        pyro.sample(
+            "mu",
+            dist.Normal(
+                pyro.param("m"), 1 / (pyro.param("beta") * lamda).sqrt()
+            ).to_event(1),
+        )
 
 
 def main(args):
     if args.cuda:
         torch.set_default_tensor_type("torch.cuda.FloatTensor")
+    global model, guide
 
-    logging.info("Loading data")
-    data = poly.load_data(poly.JSB_CHORALES)
-
-    logging.info("-" * 40)
-    model = models[args.model]
-    logging.info(
-        "Training {} on {} sequences".format(
-            model.__name__, len(data["train"]["sequences"])
-        )
+    logging.info("Data simulation")
+    pyro.set_rng_seed(args.seed)
+    pyro.clear_param_store()
+    pyro.param("rho0", torch.tensor([100, 300, 200]), constraint=constraints.positive)
+    pyro.param(
+        "alpha0",
+        torch.tensor([[20, 10, 30], [15, 25, 10], [30, 15, 20]]),
+        constraint=constraints.positive,
     )
-    sequences = data["train"]["sequences"]
-    lengths = data["train"]["sequence_lengths"]
+    pyro.param(
+        "a0", torch.tensor([10000, 10000, 10000]), constraint=constraints.positive
+    )
+    pyro.param("b0", torch.tensor([1, 1, 1]), constraint=constraints.positive)
+    pyro.param("m0", torch.tensor([0.1, 0.5, 0.8]), constraint=constraints.real)
+    pyro.param("beta0", torch.tensor([1, 1, 1]), constraint=constraints.positive)
+    #  samples = {}
+    #  samples["pi"]
+    predictive = Predictive(handlers.uncondition(model), num_samples=1)
+    data = torch.zeros(100, 32)
+    simulation = predictive(data, 3, False)
+    for t in range(data.shape[0]):
+        data[t, :] = simulation[f"x_{t}"][0].data
 
-    # find all the notes that are present at least once in the training set
-    present_notes = (sequences == 1).sum(0).sum(0) > 0
-    # remove notes that are never played (we remove 37/88 notes)
-    sequences = sequences[..., present_notes]
-
-    if args.truncate:
-        lengths = lengths.clamp(max=args.truncate)
-        sequences = sequences[:, : args.truncate]
-    num_observations = float(lengths.sum())
     pyro.set_rng_seed(args.seed)
     pyro.clear_param_store()
 
-    # We'll train using MAP Baum-Welch, i.e. MAP estimation while marginalizing
-    # out the hidden state x. This is accomplished via an automatic guide that
-    # learns point estimates of all of our conditional probability tables,
-    # named probs_*.
-    guide = AutoDelta(
-        handlers.block(model, expose_fn=lambda msg: msg["name"].startswith("probs_"))
-    )
-
-    # To help debug our tensor shapes, let's print the shape of each site's
-    # distribution, value, and log_prob tensor. Note this information is
-    # automatically printed on most errors inside SVI.
-    if args.print_shapes:
-        if args.model == "0":
-            first_available_dim = -2
-        elif args.model == "7":
-            first_available_dim = -4
-        else:
-            first_available_dim = -3
-        guide_trace = handlers.trace(guide).get_trace(
-            sequences, lengths, args=args, batch_size=args.batch_size
-        )
-        model_trace = handlers.trace(
-            handlers.replay(handlers.enum(model, first_available_dim), guide_trace)
-        ).get_trace(sequences, lengths, args=args, batch_size=args.batch_size)
-        logging.info(model_trace.format_shapes())
-
     # Bind non-PyTorch parameters to make these functions jittable.
-    model = functools.partial(model, args=args)
-    guide = functools.partial(guide, args=args)
+    #  model = functools.partial(model, args=args)
+    #  guide = functools.partial(guide, args=args)
 
     # Enumeration requires a TraceEnum elbo and declaring the max_plate_nesting.
     # All of our models have two plates: "data" and "tones".
     optimizer = optim.Adam({"lr": args.learning_rate})
-    if args.tmc:
-        if args.jit and not args.funsor:
-            raise NotImplementedError("jit support not yet added for TraceTMC_ELBO")
-        Elbo = infer.JitTraceTMC_ELBO if args.jit else infer.TraceTMC_ELBO
-        elbo = Elbo(max_plate_nesting=1 if model is model_0 else 2)
-        tmc_model = handlers.infer_config(
-            model,
-            lambda msg: {"num_samples": args.tmc_num_samples, "expand": False}
-            if msg["infer"].get("enumerate", None) == "parallel"
-            else {},
-        )  # noqa: E501
-        svi = infer.SVI(tmc_model, guide, optimizer, elbo)
-    else:
-        if args.model == "7":
-            assert args.funsor
-            Elbo = (
-                infer.JitTraceMarkovEnum_ELBO
-                if args.jit
-                else infer.TraceMarkovEnum_ELBO
-            )
-        else:
-            Elbo = infer.JitTraceEnum_ELBO if args.jit else infer.TraceEnum_ELBO
-        if args.model == "0":
-            max_plate_nesting = 1
-        elif args.model == "7":
-            max_plate_nesting = 3
-        else:
-            max_plate_nesting = 2
-        elbo = Elbo(
-            max_plate_nesting=max_plate_nesting,
-            strict_enumeration_warning=True,
-            jit_options={"time_compilation": args.time_compilation},
-        )
-        svi = infer.SVI(model, guide, optimizer, elbo)
+    Elbo = infer.JitTraceEnum_ELBO if args.jit else infer.TraceEnum_ELBO
+    max_plate_nesting = 2
+    elbo = Elbo(
+        max_plate_nesting=max_plate_nesting,
+        strict_enumeration_warning=True,
+        jit_options={"time_compilation": args.time_compilation},
+    )
+    svi = infer.SVI(model, guide, optimizer, elbo)
 
     # We'll train on small minibatches.
     logging.info("Step\tLoss")
     for step in range(args.num_steps):
-        loss = svi.step(sequences, lengths, batch_size=args.batch_size)
-        logging.info("{: >5d}\t{}".format(step, loss / num_observations))
+        loss = svi.step(data, 3, args.vectorized)
+        logging.info("{: >5d}\t{}".format(step, loss))
 
     if args.jit and args.time_compilation:
         logging.debug(
@@ -229,46 +226,8 @@ def main(args):
 
     # We evaluate on the entire training dataset,
     # excluding the prior term so our results are comparable across models.
-    train_loss = elbo.loss(
-        model,
-        guide,
-        sequences,
-        lengths,
-        batch_size=sequences.shape[0],
-        include_prior=False,
-    )
-    logging.info("training loss = {}".format(train_loss / num_observations))
-
-    # Finally we evaluate on the test dataset.
-    logging.info("-" * 40)
-    logging.info(
-        "Evaluating on {} test sequences".format(len(data["test"]["sequences"]))
-    )
-    sequences = data["test"]["sequences"][..., present_notes]
-    lengths = data["test"]["sequence_lengths"]
-    if args.truncate:
-        lengths = lengths.clamp(max=args.truncate)
-    num_observations = float(lengths.sum())
-
-    # note that since we removed unseen notes above (to make the problem a bit easier and for
-    # numerical stability) this test loss may not be directly comparable to numbers
-    # reported on this dataset elsewhere.
-    test_loss = elbo.loss(
-        model,
-        guide,
-        sequences,
-        lengths,
-        batch_size=sequences.shape[0],
-        include_prior=False,
-    )
-    logging.info("test loss = {}".format(test_loss / num_observations))
-
-    # We expect models with higher capacity to perform better,
-    # but eventually overfit to the training set.
-    capacity = sum(
-        value.reshape(-1).size(0) for value in pyro.get_param_store().values()
-    )
-    logging.info("model_{} capacity = {} parameters".format(args.model, capacity))
+    train_loss = elbo.loss(model, guide, data, 3, False)
+    logging.info("training loss = {}".format(train_loss))
 
 
 if __name__ == "__main__":
@@ -276,16 +235,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="MAP Baum-Welch learning Bach Chorales"
     )
-    parser.add_argument(
-        "-m",
-        "--model",
-        default="1",
-        type=str,
-        help="one of: {}".format(", ".join(sorted(models.keys()))),
-    )
     parser.add_argument("-n", "--num-steps", default=50, type=int)
-    parser.add_argument("-b", "--batch-size", default=8, type=int)
-    parser.add_argument("-d", "--hidden-dim", default=16, type=int)
     parser.add_argument("-nn", "--nn-dim", default=48, type=int)
     parser.add_argument("-nc", "--nn-channels", default=2, type=int)
     parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
@@ -293,18 +243,10 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--print-shapes", action="store_true")
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--cuda", action="store_true")
+    parser.add_argument("--vectorized", action="store_true")
     parser.add_argument("--jit", action="store_true")
     parser.add_argument("--time-compilation", action="store_true")
     parser.add_argument("-rp", "--raftery-parameterization", action="store_true")
-    parser.add_argument(
-        "--tmc",
-        action="store_true",
-        help="Use Tensor Monte Carlo instead of exact enumeration "
-        "to estimate the marginal likelihood. You probably don't want to do this, "
-        "except to see that TMC makes Monte Carlo gradient estimation feasible "
-        "even with very large numbers of non-reparametrized variables.",
-    )
-    parser.add_argument("--tmc-num-samples", default=10, type=int)
     parser.add_argument("--funsor", action="store_true")
     args = parser.parse_args()
 
